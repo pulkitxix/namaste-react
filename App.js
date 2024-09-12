c2be2fa750f655f3d6c7eb657aec4bebf30ecfdf
Pulkit <pulkitxi.x@gmail.com>
2024-09-12 23:26:55 +0530

    Updated

--- App.js
+++ App.js
@@ -1,11 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Namaste React"
-);
+const heading = <h1 className="head">Namaste React using JSX!</h1>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
