39ad759f8c17f7dba6124a8d5c42fae7f0c7284c
Pulkit <pulkitxi.x@gmail.com>
2024-09-12 12:14:34 +0530

    Updated

--- App.js
+++ App.js
@@ -1,32 +1,12 @@
 import React from "react";
-
 import ReactDOM from "react-dom";
 
-const parent = React.createElement(
-    "div", 
-    { id: "parent" }, 
-    [
-      React.createElement(
-        "div", 
-        { id: "child" }, 
-        [
-          React.createElement("h1", {}, "This is Namaste React"),
-          React.createElement("h2", {}, "By Akshay Saini")
-        ]
-      ),
-      React.createElement(
-        "div", 
-        { id: "child2" }, 
-        [
-          React.createElement("h1", {}, "I'm an h1 tag"),
-          React.createElement("h2", {}, "I'm an h2 tag")
-        ]
-      )
-    ]
-  );
-  
-  console.log(parent);
-  
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-  root.render(parent);
-  
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Namaste React"
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
